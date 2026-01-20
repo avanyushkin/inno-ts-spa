547ea1cc3e7841b89c68f1b7bd936d6c44043354
avanyushkin <a.vanyushkin1@gmail.com>
2026-01-20 13:49:02 +0300

    feat (feature/login): add validation to login form

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,31 +1,67 @@
-import { Button } from "@/components/ui/button.tsx";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card.tsx";
-import { Input } from "@/components/ui/input.tsx";
-import { Label } from "@/components/ui/label.tsx";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-export function LoginForm() {
+const formSchema = z.object({
+  email: z.string(),
+  password: z.string().min(8, {
+    message: "Password must be at least 8 characters.",
+  }),
+});
+const LoginForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
   return (
-    <Card className="w-full max-w-sm">
-      <CardHeader>
-        <CardTitle className="text-2xl">Login</CardTitle>
-        <CardDescription>Enter your email below login to your account</CardDescription>
-      </CardHeader>
-      <CardContent className="grid gap-4">
-        <div className="grid gap-2">
-          <Label htmlFor="email">Email</Label>
-          <Input id="email" type="email" placeholder="m@example.com" required />
-        </div>
-        <div className="grid gap-2">
-          <Label htmlFor="password">Password</Label>
-          <Input id="password" type="password" required />
-        </div>
-      </CardContent>
-      <CardFooter>
-        <div className="flex gap-4">
-          <Button className="flex-1 bg-blue-600 hover:bg-blue-700">Sign In</Button>
-          <Button variant="outline" className="flex-1">Register</Button>
-        </div>
-      </CardFooter>
-    </Card>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <Card className="w-full max-w-sm">
+          <CardHeader>
+            <CardTitle className="text-2xl">Login</CardTitle>
+            <CardDescription>Enter your email below to login to your account.</CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <FormField control={form.control} name="email"
+              render={({ field }) => (
+                <FormItem className="grid gap-4">
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="m@example.com" type="email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField control={form.control} name="password"
+              render={({ field }) => (
+                <FormItem className="grid gap-4">
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </CardContent>
+          <CardFooter>
+            <Button className="w-full">Sign in</Button>
+          </CardFooter>
+        </Card>
+      </form>
+    </Form>
   );
-}
+};
+
+export default LoginForm;
--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { LoginForm } from '@/components/LoginForm';
+import LoginForm from '@/components/LoginForm';
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/login')({
